cd95e40134205765607ffab92bfd70d2d3cf71e2
root <root@ABOKIEY>
2023-12-07 15:21:35 +0300

    2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import os
+from fabric.api import env, put, run
+
+env.hosts = ['54.236.44.210', '52.86.185.202']
+
+
+def do_deploy(archive_path):
+    """deploy archive path to the web servers"""
+    if not os.path.exists(archive_path):
+        return False
+
+    file_name = os.path.basename(archive_path)
+    dir_name = file_name.replace(".tgz", "")
+    dir_path = "/data/web_static/releases/{}/".format(dir_name)
+    success = False
+
+    try:
+        """Uploads the archive to the /tmp/ directory of theserver"""
+        put(archive_path, "/tmp/{}".format(file_name))
+        run("mkdir -p {}".format(dir_path))
+        """extract files to tmp dir"""
+        run("tar -xzf /tmp/{} -C {}".format(file_name, dir_path))
+        run("rm -rf /tmp/{}".format(file_name))
+        run("mv {}web_static/* {}".format(dir_path, dir_path))
+        run("rm -rf {}web_static".format(dir_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(dir_path))
+        print('New version deployed!')
+        success = True
+    except Exception:
+        success = False
+    return success
