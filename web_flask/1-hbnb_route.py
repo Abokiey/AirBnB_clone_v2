e3d7f24b88ac71f801232f57f734bd337251d16a
Abokiey <sweneysam@gmail.com>
2023-12-26 20:21:45 +0300

    1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""start web application with flask"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """text to display"""
+    return ('Hello HBNB!')
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """text to display"""
+    return ('HBNB')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
