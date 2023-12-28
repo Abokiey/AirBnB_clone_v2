ea78b05cf3186e7a5ba0e9e44fe16562ff75932f
Abokiey <sweneysam@gmail.com>
2023-12-28 12:30:33 +0300

    7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""start web application with flask"""
+
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """lists all states"""
+    states = storage.all("state")
+    return render_template("7-states_list.html", states=states)
+
+@app.teardown_appcontext
+def teardown(exc):
+    """close the sqlalchemy session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
